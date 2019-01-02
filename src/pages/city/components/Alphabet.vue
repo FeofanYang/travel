733751898a14d5be02ff6c63849dd8107c5cab95
FeofanYang <yyf0326@qq.com>
2019-01-02 15:40:40 +0800

    update
    - city页面 布局完成

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+		<div class="list">
+			<div class="item">A</div>
+			<div class="item">B</div>
+			<div class="item">C</div>
+			<div class="item">D</div>
+			<div class="item">E</div>
+			<div class="item">F</div>
+			<div class="item">G</div>
+		</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CityAlphabet",
+};
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/styles/var.scss";
+.list{
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	width: .4rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	color: $blue;
+}
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="list" ref="wrapper">
+      <div>
+        <div class="area">
+          <div class="title">您的位置</div>
+          <div class="button-list">
+            <div class="button van-hairline--surround">北京</div>
+          </div>
+        </div>
+        <div class="hot">
+          <div class="title">热门城市</div>
+          <div class="button-list">
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+            <div class="button van-hairline--surround">北京</div>
+          </div>
+        </div>
+        <div class="all">
+          <div class="title">A</div>
+          <div class="item-list">
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+            <div class="item van-hairline--bottom">啊拉尔</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BScroll from "better-scroll";
+export default {
+  name: "CityList",
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper);
+  },
+  data() {
+    return {
+      value: null
+    };
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/styles/var.scss";
+.list {
+  position: absolute;
+  top: 90px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: hidden;
+}
+.title {
+  padding: 0.1rem;
+  background-color: $background-color;
+  color: $text-color;
+}
+.button-list {
+  padding: 0.2rem 0.6rem 0.2rem 0.2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.button {
+  margin: 0.1rem 0;
+  padding: 0.05rem 0;
+  width: 30%;
+  text-align: center;
+  font-size: 0.22rem;
+}
+.item {
+  padding: 0.1rem;
+  width: 100%;
+  font-size: 0.22rem;
+}
+</style>
