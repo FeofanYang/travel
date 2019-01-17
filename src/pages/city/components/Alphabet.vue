425b3cd332c7f2eca8c72c801bd14108f0a6034c
FeofanYang <yyf0326@qq.com>
2019-01-17 14:17:30 +0800

    update
    - 添加IP地址访问

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -6,7 +6,7 @@
         :key="item"
         :ref="item"
         class="item"
-        @touchstart="handleTouchStart"
+        @touchstart.prevent="handleTouchStart"
         @touchmove="handleTouchMove"
         @touchend="handleTouchEnd"
         @click="handleCharClick"
